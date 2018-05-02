353fb26c391eb007d0bde8ef08c0255e22842f55
Aibek Muratbekov <aibek.muratbekov@gmail.com>
2018-05-02 18:21:39 +0600

    middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -21,16 +22,15 @@ func main() {
 
 	//set middleware
 	siteHandler := accessLogMiddleware(siteMux)
-	siteHandler = panicMeddleware(siteHandler)
+	siteHandler = panicMiddleware(siteHandler)
 
 	fmt.Println("Staarting server at :8080")
 	http.ListenAndServe(":8080", siteHandler)
 }
 
-
 // middleware
 
-func panicMeddleware(next http.Handler) http.Handler {
+func panicMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("panicMiddleware", r.URL.Path)
 		defer func() {
@@ -47,13 +47,23 @@ func panicMeddleware(next http.Handler) http.Handler {
 func accessLogMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("accessLogMiddleware", r.URL.Path)
+		start := time.Now()
 		next.ServeHTTP(w, r)
+		fmt.Printf("[%s] %s, %s %s\n", r.Method, r.RemoteAddr, r.URL.Path, time.Since(start))
 	})
 }
 
 func adminAuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("adminAuthMiddleware", r.URL.Path)
+
+		_, err := r.Cookie("session_id")
+		if err != nil {
+			fmt.Println("no auth at", r.URL.Path)
+			http.Redirect(w, r, "/", http.StatusFound)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
@@ -61,23 +71,46 @@ func adminAuthMiddleware(next http.Handler) http.Handler {
 // handler
 
 func adminIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, `<a href="/">site index </a>`)
+	fmt.Fprintln(w, "Admin main page")
 
-	fmt.Fprintf(w,"adminINdex")
 }
 
 func panicPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w,"panicPage")
+	panic("this is must me recovered")
 }
 
 func loginPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w,"loginPage")
+	expiration := time.Now().Add(10 * time.Hour)
+	cookie := http.Cookie{
+		Name:    "session_id",
+		Value:   "ludovicose",
+		Expires: expiration,
+	}
+	http.SetCookie(w, &cookie)
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 func logoutPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w,"logoutPage")
+	session, err := r.Cookie("session_id")
+	if err == http.ErrNoCookie {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	session.Expires = time.Now().AddDate(0, 0, -1)
+	http.SetCookie(w, session)
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w,"mainPage")
-}
+	session, err := r.Cookie("session_id")
+	loggedIn := (err != http.ErrNoCookie)
 
+	if loggedIn {
+		fmt.Fprintln(w, `<a href="/logout">Logout </a>`)
+		fmt.Fprintln(w, "Welcome, "+session.Value)
+	} else {
+		fmt.Fprintln(w, `<a href="/login">Login </a>`)
+		fmt.Fprintln(w, "You nedd to login")
+	}
+}
