fa7bcd80252dec9daa8ecb7caba2e635819b199f
Aibek Muratbekov <aibek.muratbekov@gmail.com>
2018-05-03 20:22:21 +0600

    session

---
+++ session/session.go
@@ -0,0 +1,135 @@
+package session
+
+import (
+	"sync"
+	"fmt"
+	"io"
+	"crypto/rand"
+	"encoding/base64"
+	"net/http"
+	"net/url"
+	"time"
+	"html/template"
+)
+
+//Global session manager.
+type Manager struct {
+	cookieName  string
+	lock        sync.Mutex
+	provider    Provider
+	maxlifetime int64
+}
+
+//Unique session id's
+func (manager *Manager) sessionId() string {
+	b := make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		return ""
+	}
+	return base64.URLEncoding.EncodeToString(b)
+}
+
+func (manager *Manager) SessionStart(w http.ResponseWriter, r *http.Request) (session Session) {
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+
+	cookie, err := r.Cookie(manager.cookieName)
+	if err != nil || cookie.Value == "" {
+		sid := manager.sessionId()
+		session, _ = manager.provider.SessionInit(sid)
+		cookie := http.Cookie{Name: manager.cookieName, Value: url.QueryEscape(sid), Path: "/", HttpOnly: true, MaxAge: int(manager.maxlifetime)}
+		http.SetCookie(w, &cookie)
+	} else {
+		sid, _ := url.QueryUnescape(cookie.Value)
+		session, _ = manager.provider.SessionRead(sid)
+	}
+	return
+}
+
+func (manager *Manager) SessionDestroy(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie(manager.cookieName)
+	if err != nil || cookie.Value == "" {
+		return
+	} else {
+		manager.lock.Lock()
+		defer manager.lock.Unlock()
+		manager.provider.SessionDestroy(cookie.Value)
+		expiration := time.Now()
+		cookie := http.Cookie{Name: manager.cookieName, Path: "/", HttpOnly: true, Expires: expiration, MaxAge: -1}
+		http.SetCookie(w, &cookie)
+	}
+}
+
+func (manager *Manager) GC() {
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+	manager.provider.SessionGC(manager.maxlifetime)
+	time.AfterFunc(time.Duration(manager.maxlifetime), func() {
+		manager.GC()
+	})
+}
+
+type Provider interface {
+	SessionInit(sid string) (Session, error) // implements the initialization of a session, and returns a new session if it succeeds
+	SessionRead(sid string) (Session, error) // returns a session represented by the corresponding sid. Creates a new session and returns it if it does not already exist.
+	SessionDestroy(sid string) error         // given an sid, deletes the corresponding session
+	SessionGC(maxLifeTime int64)             // deletes expired session variables according to maxLifeTime
+}
+
+type Session interface {
+	Set(key, value interface{}) error //set session value
+	Get(key interface{}) interface{}  //get session value
+	Delete(key interface{}) error     //delete session value
+	SessionID() string                //back current sessionID
+}
+
+var provides = make(map[string]Provider)
+
+func NewManager(provideName, cookieName string, maxlifetime int64) (*Manager, error) {
+	provider, ok := provides[provideName]
+	if !ok {
+		return nil, fmt.Errorf("session: unknown provide %q (forgotten import?)", provideName)
+	}
+	return &Manager{provider: provider, cookieName: cookieName, maxlifetime: maxlifetime}, nil
+}
+
+func Register(name string, provider Provider) {
+	if provider == nil {
+		panic("session: Register provider is nil")
+	}
+	if _, dup := provides[name]; dup {
+		panic("session: Register called twice for provider " + name)
+	}
+
+	provides[name] = provider
+}
+
+var globalSession *Manager
+
+func init() {
+
+	globalSession, _ = NewManager("memory", "gosessionid", 3600)
+	go globalSession.GC()
+
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	sess := globalSession.SessionStart(w, r)
+	createtime := sess.Get("createtime")
+	if createtime == nil {
+		sess.Set("createtime", time.Now().Unix())
+	} else if (createtime.(int64) + 360) < (time.Now().Unix()) {
+		globalSession.SessionDestroy(w, r)
+		sess = globalSession.SessionStart(w, r)
+	}
+	ct := sess.Get("countnum")
+	if ct == nil {
+		sess.Set("countnum", 1)
+	} else {
+		sess.Set("countnum", (ct.(int) + 1))
+	}
+
+	t, _ := template.ParseFiles("count.gtpl")
+	w.Header().Set("Content-Type", "text/html")
+	t.Execute(w, sess.Get("countnum"))
+}
