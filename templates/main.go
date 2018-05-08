8c74fb9f95305b89f61e9efc2a5e0d456603205f
Ludovicose <aibek.muratbekov@gmail.com>
2018-05-08 21:01:11 +0600

    templates fields

---
+++ templates/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type Person struct {
+	UserName string
+}
+
+func main() {
+	t := template.New("fieldname example")
+	t, _ = t.Parse("hello {{.UserName}}!")
+	p := Person{UserName: "Ludovicose"}
+	t.Execute(os.Stdout, p)
+}
